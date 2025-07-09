1bf8374aa200a9faaac9bd0c4d1dc411f8f4eb22
prop11 <alex.woods421@gmail.com>
2025-07-09 13:10:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-if __name__ == "__main__":
-    import tkinter as tk
-    from src.mud_client_app import MUDClientApp  # Updated import
+import tkinter as tk
+from src.mud_client_app import MUDClientApp # Import MUDClientApp from the src package
 
+if __name__ == "__main__":
     root = tk.Tk()
     app = MUDClientApp(root)
     root.mainloop()
